bbf17b36bf4b26c0e894ce771f6f8f42d377096d
ArturAmbartsumov <Artur_pskov@mail.ru>
2014-05-06 00:47:47 +0400

    Незначительные изменения

--- main/views.py
+++ main/views.py
@@ -10,6 +10,8 @@ def index(request):
 def article(request, article):
 	#article = get_object_or_404(Articles, short_name = article)
 	#return render(request, 'main/' + article.template, {'article': article})
+	if article == '':
+		return render(request, 'main/article.html')
 	return render(request, 'main/article.html')
 
 def articleList(request):
@@ -20,11 +22,19 @@ def articleList(request):
 def department(request, department):
 	#department = get_object_or_404(Departments, short_name = department)
 	#return render(request, 'main/' + department.template, {'department': department})
+	if department == 'lstp':
+		return render(request, 'main/lstp.html')
+	if department == 'RA':
+		return render(request, 'main/RA.html')
+	if department == 'buamzem':
+		return render(request, 'main/buamzem.html')
 	return render(request, 'main/department.html')
 
 def project(request, project):
 	#project = get_object_or_404(Projects, short_name = project)
 	#return render(request, 'main/' + project.template, {'project': project})
+	if project == '':
+		return render(request, 'main/project.html')
 	return render(request, 'main/project.html')
 
 def projectList(request):
@@ -35,6 +45,10 @@ def projectList(request):
 def news(request, news_id):
 	#project = get_object_or_404(Projects, short_name = project)
 	#return render(request, 'main/' + project.template, {'project': project})
+	if news_id == 1:
+		return render(request, 'main/news1.html')
+	if news_id == 2:
+		return render(request, 'main/news2.html')
 	return render(request, 'main/news.html')
 
 def newsList(request):
