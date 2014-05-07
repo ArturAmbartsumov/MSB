c83a60875b23f82cd5918ad402815b23adfd772c
ArturAmbartsumov <Artur_pskov@mail.ru>
2014-05-07 23:22:31 +0400

    Исправлены вьюшки

--- main/views.py
+++ main/views.py
@@ -1,30 +1,56 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 from main.models import *
+import datetime
 
 #http://djbook.ru/rel1.6/topics/db/queries.html
 
 def index(request):
 	#articleList = get_list_or_404(ArticleDescriptions)
 	#return render(request, 'main/index.html', {'articleList': articleList})
-	lastNews = get_list_or_404(News)
-	return render(request, 'main/index.html')
+	#lastNews = get_list_or_404(News, pub_date__lte=datetime.datetime.now(), isFavorite=True).order_by('pub_date')[:5]
+	newsList = News.objects.filter(pub_date__lte = datetime.datetime.now(), isFavorite = True).order_by('-pub_date')[:5]
+	departmentsList = Departments.objects.exclude(short_name = 'msb')
+	projectsList = Projects.objects.filter(department__short_name = 'msb')[:5]
+	context = {
+		'newsList': newsList,
+		'departmentsList': departmentsList,
+		'projectsList': projectsList,
+	}
+	return render(request, 'main/index.html', context)
 
 def article(request, article):
 	#article = get_object_or_404(Articles, short_name = article)
 	#return render(request, 'main/' + article.template, {'article': article})
+	article = get_object_or_404(Articles, short_name = article)
+	context = {
+		'article': article
+	}
 	if article == 'article':
-		return render(request, 'main/article.html')
-	return render(request, 'main/article.html')
+		return render(request, 'main/article.html', context)
+	return render(request, 'main/article.html', context)
 
 def articleList(request):
 	#articleList = get_list_or_404(ArticleDescriptions)
 	#return render(request, 'main/articles.html', {'articleList': articleList})
-	return render(request, 'main/articles.html')
+	articlesList = Articles.objects.filter(pub_date__lte = datetime.datetime.now()).order_by('-pub_date')[:10]
+	context = {
+		'articlesList': articlesList
+	}
+	return render(request, 'main/articles.html', articlesList)
 
 def department(request, department):
 	#department = get_object_or_404(Departments, short_name = department)
 	#return render(request, 'main/' + department.template, {'department': department})
+	cur_department = get_object_or_404(Departments, short_name = department)
+	projectsList = cur_department.projects_set.all()[:5]
+	articlesList = cur_department.article.filter(pub_date__lte = datetime.datetime.now(), isFavorite = True).order_by('-pub_date')[:5]
+	context = {
+		'department': cur_department,
+		'projectsList': projectsList,
+		'articlesList': articlesList
+	}
+	print context
 	if department == 'lstp':
 		return render(request, 'main/lstp.html')
 	if department == 'RA':
