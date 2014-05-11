f104346c3c96aa87490379011d2b19bb00b2540d
Anastasia-sweet <naa92@mail.ru>
2014-05-11 21:02:31 +0400

    Шаблоны Джанго часть 2
    Немного инфы в БД + индекс в шаблонах

--- main/static/main/js/main.js
+++
@@ -1,9 +0,0 @@
-$('.projectsGallerie_project').hover(showText, hideText);
-
-function showText(){
-	textDiv = $('.projectsGallerie_project_text', this).slideDown('fast');
-}
-
-function hideText(){
-	textDiv = $('.projectsGallerie_project_text', this).slideUp('fast');
-}
