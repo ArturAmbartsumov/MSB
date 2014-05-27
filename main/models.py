505958d8b7086f75fa373920402554dfc183abd7
ArturAmbartsumov <Artur_pskov@mail.ru>
2014-05-27 21:59:49 +0400

    Revert "Revert "Галерея на главную. Изменены модели.""
    This reverts commit a8e0033e1b1d0a2a4f873f5ec2d4609e506cab3c.

--- main/models.py
+++ main/models.py
@@ -13,6 +13,7 @@ class Departments(models.Model):
 	photo = models.ForeignKey(Photo, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
 	gallery = models.ForeignKey(Gallery, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
 	article = SortedManyToManyField('Articles', blank = True)
+	content = RichTextField()
 	template = models.CharField(max_length=50, default='base_department.html')
 	def __unicode__(self):
 		return self.short_name
@@ -65,7 +66,7 @@ class AboutBMSTU(models.Model):
 	gallery = models.ForeignKey(Gallery, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
 	article = SortedManyToManyField(Articles, blank = True)
 	history_title = models.CharField(max_length=250)
-	history_text = models.TextField(blank=True)
+	history_text = RichTextField()
 	history_photo = models.ForeignKey(Photo, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
 	history_gallery = models.ForeignKey(Gallery, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
 	nowadays_title = models.CharField(max_length=250)
@@ -77,6 +78,21 @@ class AboutBMSTU(models.Model):
 	def __unicode__(self):
 		return self.title
 
+class Phones(models.Model):
+	phone_number = models.CharField(max_length=250)
+	def __unicode__(self):
+		return self.phone_number
+
+class Emails(models.Model):
+	email = models.CharField(max_length=250)
+	def __unicode__(self):
+		return self.email
+
+class Addresses(models.Model):
+	address = models.CharField(max_length=250)
+	def __unicode__(self):
+		return self.address
+
 class Persons(models.Model):
 	full_name = models.CharField(max_length=250)
 	text = models.TextField(blank=True)
@@ -87,8 +103,9 @@ class Persons(models.Model):
 class Contacts(models.Model):
 	title = models.CharField(max_length=250)
 	photo_map = models.ForeignKey(Photo, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
-	phone_number = models.CharField(max_length=250, blank=True)
-	adress = models.CharField(max_length=250, blank=True)
+	phone_number = SortedManyToManyField(Phones, blank = True)
+	addresses = SortedManyToManyField(Addresses, blank = True)
+	emails = SortedManyToManyField(Emails, blank = True)
 	contact_information = models.TextField(blank=True)
 	contactees = SortedManyToManyField(Persons, blank = True)
 	def __unicode__(self):
