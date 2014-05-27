4564c6b863fd6d218156a0b01e2381a9470a4acf
Anastasia-sweet <naa92@mail.ru>
2014-05-27 22:22:01 +0400

    Обновление -1
    Главная, списки, контакты

--- main/static/main/js/customScript.js
+++ main/static/main/js/customScript.js
@@ -7,3 +7,22 @@ function showText(){
 function hideText(){
 	textDiv = $('.projectsGallerie_project_text', this).slideUp('fast');
 }
+
+/*------------------------------------------Progect-Carousell-Many-Items------------------------
+
+$('.carousel[data-type="multi"] .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});*/
