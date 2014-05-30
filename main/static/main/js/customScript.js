349d90e3f9adc28c4cec634ba575870dcb80ae12
ArturAmbartsumov <Artur_pskov@mail.ru>
2014-05-30 23:51:54 +0400

    Смена стиля меню при прокрутке

--- main/static/main/js/customScript.js
+++ main/static/main/js/customScript.js
@@ -8,23 +8,15 @@ function hideText(){
   textDiv = $('.projectsGallerie_project_text', this).slideUp('fast');
 }
 
-/*------------------------------------------Progect-Carousell-Many-Items------------------------
-
-$('.carousel[data-type="multi"] .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  for (var i=0;i<2;i++) {
-    next=next.next();
-    if (!next.length) {
-    	next = $(this).siblings(':first');
-  	}
-    
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});*/
+$(window).scroll(function(){
+    var windowpos = $(window).scrollTop();
+    if(windowpos > 50) {
+        $('#trensperent-menu').hide();
+        $('#fixed-menu').show();
+    } else {
+        $('#fixed-menu').hide();
+        $('#trensperent-menu').show();
+    }
+});
 
 
